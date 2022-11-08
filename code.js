8883a2158bd9e5922f1ccd3e9e0c03323673135a
Aurange <11928004+TheAurange@users.noreply.github.com>
2022-11-08 02:55:53 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,13 @@
+document.querySelectorAll("nav#primary > ul > li").forEach(e => {
+	e.addEventListener("click", function(evt){
+		if(this.id !== "primary-selected"){
+			document.querySelector("section.main#" + document.querySelector("nav#primary > ul > li#primary-selected > span").innerText.replace(" ", "").toLowerCase()).style.display = "none";
+			document.querySelector("nav#primary > ul > li#primary-selected").removeAttribute("id");
+
+			this.id = "primary-selected";
+			document.querySelector("section.main#" + this.children[0].innerText.replace(" ", "").toLowerCase()).style.display = "block";
+		}
+	});
+});
+
+document.querySelector("section#footer").innerText = document.querySelector("section#footer").innerText.replace(" Aurange", "") + (new Date().getFullYear() > 2022 ? " - " + new Date().getFullYear() + " Aurange" : " Aurange");
