0d11acb67a97f97a0edae10ad72ca4a80471f69e
Aurange <11928004+TheAurange@users.noreply.github.com>
2024-09-01 21:29:48 -0500

    Update code.js

--- code.js
+++ code.js
@@ -18,25 +18,22 @@ fetch("https://api.github.com/users/TheAurange/repos", {
         	if(!e.name.match(/theaurange/i)){
 			let tempTitle = document.createElement("span"),
 			    tempType = document.createElement("span"),
-			    tempButton = document.createElement("button"),
-			    tempA = document.createElement("a"),
-			    tempElem = document.createElement("div");
+			    tempElem = document.createElement("div"),
+			    tempA = document.createElement("a");
 
 			tempTitle.classList.add("itemTitle");
 			tempTitle.innerText = e.name.replace(/-/g, " ").replace(/(^\w)|(\s\w)/g, l => l.toUpperCase()).replace("Gmail", "GMail");
 
 			tempType.innerText = (e.topics.indexOf("userscript") === -1) ? ` ${String.fromCodePoint(0x1F4A1)}` : ` ${String.fromCodePoint(0x1F435)}`;
 
-			tempButton.innerText = "Repo";
+			tempElem.classList.add("item");
+			tempElem.append(tempTitle, tempType, document.createElement("br"), document.createElement("br"), e.description);
 
 			tempA.href = e.html_url + "/";
 			tempA.target = "_blank";
-			tempA.append(tempButton);
+			tempA.append(tempElem);
 
-			tempElem.classList.add("item");
-			tempElem.append(tempTitle, tempType, document.createElement("br"), document.createElement("br"), e.description, document.createElement("br"), document.createElement("br"), tempA);
-
-			document.querySelector("section#wrap").append(tempElem);
+			document.querySelector("section#wrap").append(tempA);
         	}
 	});
 });
