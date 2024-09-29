fdc255be564d9dad535c1c1321dd5895cf6e7528
Aurange <11928004+TheAurange@users.noreply.github.com>
2024-09-29 16:45:19 -0500

    Update code.js

--- code.js
+++ code.js
@@ -17,19 +17,13 @@ fetch("https://api.github.com/users/TheAurange/repos", {
 	data.forEach(e => {
         	if(!e.name.match(/theaurange/i)){
 			let tempTitle = document.createElement("span"),
-			    tempType = document.createElement("span"),
-			    tempP = document.createElement("p"),
 			    tempElem = document.createElement("div"),
 			    tempA = document.createElement("a");
 
 			tempTitle.innerText = e.name.replace(/-/g, " ").replace(/(^\w)|(\s\w)/g, l => l.toUpperCase()).replace("Gmail", "GMail");
 
-			tempType.innerText = (e.topics.indexOf("userscript") === -1) ? `${String.fromCodePoint(0x1F4A1)}` : `${String.fromCodePoint(0x1F435)}`;
-
-			tempP.append(tempTitle, tempType);
-
 			tempElem.classList.add("item");
-			tempElem.append(tempP, e.description);
+			tempElem.append(tempTitle, e.description);
 
 			tempA.href = e.html_url + "/";
 			tempA.target = "_blank";
